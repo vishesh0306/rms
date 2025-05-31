2856cdddfebfcf1f5dce3cb25e50c82db36b07ec
satya pathak <sp0706003@gmail.com>
2025-05-31 16:38:56 +0530

    Report Service added

--- src/main/java/com/abes/rms/service/ReportService.java
+++ src/main/java/com/abes/rms/service/ReportService.java
@@ -1,5 +1,26 @@
 package com.abes.rms.service;
 
-public class ReportService {
+import com.abes.rms.daoImp.*;
+import com.abes.rms.dto.Room;
 
+public class ReportService {
+	public Room maxBookedRoom() {
+		int maxi = 0;
+		Room temp = null;
+		for(Room room : new RoomImpDAO().getAllRooms()) {
+			if(room.getCount() > maxi) {
+				maxi = Math.max(maxi, room.getCount());
+				temp = room;
+			}
+		}
+		return temp;
+	}
+	
+	public int totalBooking() {
+		return new BookingImpDAO().getNoOfBookings();
+	}
+	
+	public double revenue() {
+		return new BookingImpDAO().calcRevenue();
+	}
 }
