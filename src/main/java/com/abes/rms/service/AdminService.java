d714a5ee35d8e84fd69f204621a8e6c1117937c9
RiddhimaaSingh <118214685+RiddhimaaSingh@users.noreply.github.com>
2025-05-31 16:28:53 +0530

    AdminService added

--- src/main/java/com/abes/rms/service/AdminService.java
+++ src/main/java/com/abes/rms/service/AdminService.java
@@ -1,5 +1,31 @@
 package com.abes.rms.service;
 
-public class AdminService {
+import java.util.ArrayList;
 
+import com.abes.rms.daoImp.*;
+import com.abes.rms.dto.*;
+
+public class AdminService {
+	AdminImpDAO adminObj = new AdminImpDAO();
+	BookingImpDAO bookingObj = new BookingImpDAO();
+	UserImpDAO userObj = new UserImpDAO();
+	
+	public boolean verifyUser(String userID, String pass) {
+		return adminObj.isAdminPresent(userID, pass);
+	}
+	public Admin getUser(String userID, String pass) {
+		return adminObj.getAdmin(userID, pass);
+	}
+	
+	public ArrayList<RegularUser> showAllUsers() {
+		return adminObj.showUsers();
+	}
+	
+	public ArrayList<Booking> showAllBookings() {
+		return bookingObj.showBookings();
+	}
+	
+	public boolean deleteUser(RegularUser user) {
+		return userObj.deleteUser(user);
+	}
 }
